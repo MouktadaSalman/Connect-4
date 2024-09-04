d4b8a8c8e28d88d145a2b46ca2c109823d9b923d
MJQ <21494299@student.curtin.edu.au>
2024-09-04 20:23:15 +0800

    Add player data class to store player info

---
+++ app/src/main/java/com/example/connect4/PlayerData.java
@@ -0,0 +1,118 @@
+package com.example.connect4;
+
+/*---------------------------------------------*
+ *  Module: PlayerData                         *
+ *  Description: Holds the players info/stats  *
+ *  Author: Jauhar                             *
+ *  ID: 21494299                               *
+ *  Version: 1.0.0                             *
+ *---------------------------------------------*/
+
+public class PlayerData {
+    /* Class Fields:
+     * playerName -> the players name
+     * pAvatar -> the players avatar
+     * wins -> the player wins (both against ai + other player)
+     * loss -> the player loss (both against ai + other player)
+     * totalGames -> the player games played (both against ai + other player)
+     * winPercent -> the player winPercent (both against ai + other player)
+     */
+    private final String playerName;
+    private final String playerAvatar;
+    private int wins;
+    private int loss;
+    private int totalGames;
+    private double winPercent;
+
+    /*----------------------------------------------*
+     * Constructor: PlayerData                      *
+     * Description: The constructor of the class    *
+     * Parameters: pName (string), pAvatar (string) *
+     *----------------------------------------------*/
+    public PlayerData(String pName, String pAvatar){
+        playerName = pName;
+        playerAvatar = pAvatar;
+        wins = 0;
+        loss = 0;
+        totalGames = 0;
+        winPercent = 0.0;
+    }
+
+    /*----------------------------------------------*
+     * Method: getPlayerName                        *
+     * Description: Get player's username           *
+     * Parameters: none                             *
+     * Result: playerName (String)                  *
+     *----------------------------------------------*/
+    public String getPlayerName(){
+        return playerName;
+    }
+
+    /*----------------------------------------------*
+     * Method: getPlayerAvatar                      *
+     * Description: Get player's avatar             *
+     * Parameters: none                             *
+     * Result: playerAvatar (String)                *
+     *----------------------------------------------*/
+    public String getPlayerAvatar(){
+        return playerAvatar;
+    }
+
+    /*----------------------------------------------*
+     * Method: addWin                               *
+     * Description: Add a win for player            *
+     * Parameters: none                             *
+     * Result: none                                 *
+     *----------------------------------------------*/
+    public void addWin(){
+        wins++;
+        totalGames++;
+
+        //Adjust win ratio
+        winPercent = (double)wins/(double)totalGames;
+    }
+
+    /*----------------------------------------------*
+     * Method: addLoss                              *
+     * Description: Add a loss for player           *
+     * Parameters: none                             *
+     * Result: none                                 *
+     *----------------------------------------------*/
+    public void addLoss(){
+        loss++;
+        totalGames++;
+
+        //Adjust win ratio
+        winPercent = (double)wins/(double)totalGames;
+    }
+
+    /*----------------------------------------------*
+     * Method: getWins                              *
+     * Description: Get player's wins               *
+     * Parameters: none                             *
+     * Result: wins (int)                           *
+     *----------------------------------------------*/
+    public int getWins(){
+        return wins;
+    }
+
+    /*----------------------------------------------*
+     * Method: getLoss                              *
+     * Description: Get player's loss               *
+     * Parameters: none                             *
+     * Result: loss (int)                           *
+     *----------------------------------------------*/
+    public int getLoss(){
+        return loss;
+    }
+
+    /*----------------------------------------------*
+     * Method: winPercentage                        *
+     * Description: Get player's win ratio          *
+     * Parameters: none                             *
+     * Result: winPercent (double)                  *
+     *----------------------------------------------*/
+    public double winPercentage(){
+        return winPercent;
+    }
+}
