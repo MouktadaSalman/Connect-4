b23c1d2201f190da76e0e014ccfbf0589854ff69
MJQ <21494299@student.curtin.edu.au>
2024-09-06 22:48:14 +0800

    Add player fields to game data

--- app/src/main/java/com/example/connect4/GameData.java
+++ app/src/main/java/com/example/connect4/GameData.java
@@ -4,14 +4,20 @@ import androidx.lifecycle.MediatorLiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.connect4.PlayerOperations.Player;
+
 public class GameData extends ViewModel {
 
     public MutableLiveData<Integer> displayedFragment;
     public MutableLiveData<Integer> selectedGameMode;
+    public MutableLiveData<Player> player1;
+    public MutableLiveData<Player> player2;
 
     public GameData(){
         displayedFragment = new MediatorLiveData<>();
         selectedGameMode = new MediatorLiveData<>();
+        player1 = new MediatorLiveData<>();
+        player2 = new MediatorLiveData<>();
     }
 
     public int getDisplayedFragment() {return displayedFragment.getValue();}
