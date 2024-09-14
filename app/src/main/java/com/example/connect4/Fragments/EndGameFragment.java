28a0217d12ad01f0e81240558588cb01ae62c747
Ahmedo-o <a8y08888@gmail.com>
2024-09-15 00:00:40 +0800

    Some changes in profile customization and in EndGameFragment

--- app/src/main/java/com/example/connect4/Fragments/EndGameFragment.java
+++ app/src/main/java/com/example/connect4/Fragments/EndGameFragment.java
@@ -2,21 +2,47 @@ package com.example.connect4.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
+import com.example.connect4.GameData;
 import com.example.connect4.R;
 
 
 public class EndGameFragment extends Fragment {
 
+    private GameData gameDataViewModel;
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_end_game, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_end_game, container, false);
+        gameDataViewModel = new ViewModelProvider(requireActivity()).get(GameData.class);
+
+
+        ImageButton exitButton = view.findViewById(R.id.EGExitButton);
+        ImageButton restartButton = view.findViewById(R.id.EGRestartButton);
+
+
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gameDataViewModel.setDisplayedFragment(0);
+            }
+        });
+
+        restartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gameDataViewModel.setDisplayedFragment(1);
+            }
+        });
+
+        return view;
     }
 }
